0aaf075387a97332e928d21cfd64805b3c91f499
UWAYONoella <noellawayo2019@gmail.com>
2023-04-03 15:13:30 +0200

    study java

---
+++ java/hello.java
@@ -0,0 +1,30 @@
+public class hello{
+    public static void main(String [] args){
+        System.out.println("Hello world!");
+       int a=4;
+        int b=3;
+        int sum= a+b;
+        System.out.println(sum);
+        String d="My name: ";
+        String c="Noella";
+        String me=d+c;
+        System.out.println(me);
+        int age=18;
+        int btn=23;
+        int check=65;
+        int marks=65;
+        if (marks<50){
+       System.out.println("FAIL!!");     
+        }
+        else if (marks==65){
+         System.out.println("D grade");    
+        }
+        else if (marks>=60 && marks<70){
+         System.out.println("C grade");    
+        }
+        else if (marks>=70 && marks<80){
+         System.out.println("B grade");    
+        }
+        
+    }
+}
---
+++ java/obj.java
@@ -0,0 +1,40 @@
+// public class obj{
+//    String  name = "Katushabe";
+
+//     public static void main(String[] args){
+//         obj myObj = new obj();
+//         System.out.println(myObj.name);
+
+// int i=1;
+// while(i<=100){
+//     System.out.println("I love You");
+//     i++;
+// }
+    
+//     }
+    
+// }
+
+
+// class Sorry{
+//     public static void main(String[] args){
+//         int x;
+//         for(x=0; x<=1000; x++){
+//             System.out.println("Sorry Kate");
+//         }
+//     }
+// }
+
+
+import java.util.Scanner;
+ class Scn {
+    public static void main(String [] args){
+        int number;
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Enter number: ");
+        // for( number=0;number<=200;number++)
+        int nr=scanner.nextInt();
+        System.out.println("The number are: , " + nr + "!");
+
+    }
+}
