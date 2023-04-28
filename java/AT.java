bc2edcc942e3c609378d50b75ad02f385a1944a0
UWAYONoella <noellawayo2019@gmail.com>
2023-04-28 16:31:02 +0200

    study

--- java/AT.java
+++ java/AT.java
@@ -1,40 +1,19 @@
-import java.util.Scanner;
-
+// import java.util.Scanner;
+class AT{
+    String accnam, accnum,acctype;
+    int accbalance;
+}
 public class AT {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int balance = 1000; // initial account balance
+       
 
-        System.out.println("Welcome to the ATM!");
-        System.out.println("Please select an option:");
-        System.out.println("1. Check Balance");
-        System.out.println("2. Withdraw");
-        System.out.println("3. Deposit");
-        System.out.println("4. Exit");
 
-        int choice = input.nextInt();
+    AT act1=new AT();
+    act1.accnam="BK";
+    act1.accnum=12688;
+    act1.acctype="saving";
+    act1.accbalance=200000;
 
-        if (choice == 1) {
-            System.out.println("Your balance is $" + balance);
-        } else if (choice == 2) {
-            System.out.println("How much would you like to withdraw?");
-            int withdrawalAmount = input.nextInt();
-            if (withdrawalAmount > balance) {
-                System.out.println("Insufficient funds.");
-            } else {
-                // balance -= withdrawalAmount;
-                balance=balance-withdrawalAmount;
-                System.out.println("Your new balance is $" + balance);
-            }
-        } else if (choice == 3) {
-            System.out.println("How much would you like to deposit?");
-            int depositAmount = input.nextInt();
-            balance += depositAmount;
-            System.out.println("Your new balance is $" + balance);
-        } else if (choice == 4) {
-            System.out.println("Thank you for using the ATM!");
-        } else {
-            System.out.println("Invalid choice.");
-        }
+
     }
 }
